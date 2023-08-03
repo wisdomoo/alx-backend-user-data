8c5895c1f00d056342b3d288645341a12d0365c6
okoroafor wisdom onyinye <104413525+wisdomoo@users.noreply.github.com>
2023-08-03 21:45:51 +0100

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,4 +13,4 @@ def hash_password(password: str) -> bytes:
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """Checks is a hashed password was formed from the given password.
     """
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)￼Enter
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
